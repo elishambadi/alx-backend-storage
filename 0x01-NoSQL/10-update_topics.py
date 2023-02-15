f42b3209865b34d3e26fbc2fe0a893e3b352a495
elishambadi <embadi43@gmail.com>
2023-02-15 05:50:05 +0300

    add task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+MongoDB methods
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics taught in a certain school
+    """
+    filter = {
+        name: name
+    }
+    mongo_collection.update(filter, {$set: {topics: topics}});
