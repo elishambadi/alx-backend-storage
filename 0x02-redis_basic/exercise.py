0b4e308e79ac49952200d3645edaf82b0f9312a2
elishambadi <embadi43@gmail.com>
2023-02-17 07:55:05 +0300

    annotate

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Using Redis in Python
 """
 import redis
 import uuid
-
+from typing import Callable
 
 class Cache:
     """
@@ -14,7 +14,20 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
-        id = str(uuid.uuid4())
+    def store(self, data) -> str:
+        id: str = str(uuid.uuid4())
         self._redis.set(id, data)
+        self._redis.incr(id)
         return id
+
+    def get(self, key: str, fn: Callable = None):
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str):
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str):
+        return self.get(key, fn=lambda x: int(x))
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -8,7 +8,7 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
+data = 100
 key = cache.store(data)
 print(key)
 
